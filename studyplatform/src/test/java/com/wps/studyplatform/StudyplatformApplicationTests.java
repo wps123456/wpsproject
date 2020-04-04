f2f44be4c2d9899646cb5150604017c72bcd0b0e
wps <1169114186@qq.com>
2020-04-04 22:48:16 +0800

    增加studyplatform子项目

--- studyplatform/src/test/java/com/wps/studyplatform/StudyplatformApplicationTests.java
+++ studyplatform/src/test/java/com/wps/studyplatform/StudyplatformApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.wpsproject;
+package com.wps.studyplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WpsprojectApplicationTests {
+class StudyplatformApplicationTests {
 
     @Test
     void contextLoads() {
