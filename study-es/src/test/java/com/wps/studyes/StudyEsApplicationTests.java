2bcff23cdd8d40f7e483540da4e72452a65e88e4
wps <1169114186@qq.com>
2020-04-07 22:36:46 +0800

    创建ES空项目

---
+++ study-es/src/test/java/com/wps/studyes/StudyEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wps.studyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
