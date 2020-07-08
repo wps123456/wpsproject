de34897235f86d5ad28bcd27e443497297a36ad1
wps <1169114186@qq.com>
2020-07-08 23:04:04 +0800

    studyplatform 增加 定时任务

--- studyplatform/src/main/java/com/wps/studyplatform/job/scheduler/DynamicJob.java
+++ studyplatform/src/main/java/com/wps/studyplatform/job/scheduler/DynamicJob.java
@@ -1,9 +1,8 @@
 package com.wps.studyplatform.job.scheduler;
 
-import org.quartz.Job;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.TriggerKey;
+import org.quartz.*;
+
+import java.util.Map;
 
 /**
  * @Title DynamicJob
@@ -14,7 +13,7 @@ import org.quartz.TriggerKey;
 public class DynamicJob {
     //要执行类，实现job接口
     private Class<? extends Job> target;
-    private String cronExperssion;
+    private String cronExpression;
     private String jobGroup= Scheduler.DEFAULT_GROUP;
     private String jobName;
 
@@ -36,7 +35,7 @@ public class DynamicJob {
         return this;
     }
     public DynamicJob cronExpression (String cronExperssion){
-        this.cronExperssion=cronExperssion;
+        this.cronExpression=cronExperssion;
         return this;
     }
     public String jobGroup(){
@@ -46,5 +45,74 @@ public class DynamicJob {
         this.jobGroup=jobGroup;
         return this;
     }
+    public String jobName(){
+        return jobName;
+    }
+    public DynamicJob jobName(String jobName){
+        this.jobName=jobName;
+        return this;
+    }
+    public TriggerKey triggerKey(){
+        if (triggerKey==null){
+            triggerKey=TriggerKey.triggerKey(this.jobName,this.jobGroup);
+        }
+        return triggerKey;
+    }
+    public JobDetail jobDetail(){
+        if (jobDetail==null){
+            jobDetail= JobBuilder.newJob(target)
+                    .withIdentity(this.jobName,this.jobGroup)
+                    .build();
+        }
+        return jobDetail;
+    }
 
+    /**
+     * 传参给执行的job
+     * 在job中通过context.getMergedJobDataMap().get(key)获取值
+     * @param key
+     * @param value
+     * @return
+     */
+    public DynamicJob addJobData(String key,Object value){
+        final JobDetail detail=jobDetail();
+        final JobDataMap jobDataMap=detail.getJobDataMap();
+        jobDataMap.put(key,value);
+        return this;
+    }
+
+    /**
+     * 传参给执行的job
+     * 在job中通过context.getMerfedJobDataMap().get(key)获取值
+     * @param map
+     * @return
+     */
+    public DynamicJob addJobDataMap(Map<String,Object> map){
+        final JobDetail detail=jobDetail();
+        final JobDataMap jobDataMap=detail.getJobDataMap();
+        jobDataMap.putAll(map);
+        return this;
+
+    }
+    public String cronExpression(){
+        return this.cronExpression;
+    }
+    public CronTrigger cronTrigger(){
+        final CronScheduleBuilder cronScheduleBuilder=CronScheduleBuilder.cronSchedule(this.cronExpression);
+        return TriggerBuilder.newTrigger().withIdentity(triggerKey())
+                .withSchedule(cronScheduleBuilder)
+                .build();
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb=new StringBuilder();
+        sb.append("{target=").append(target);
+        sb.append(", cronExperssion='").append(cronExpression).append('\'');
+        sb.append(", jobGroup='").append(jobGroup).append('\'');
+        sb.append(", jobName='").append(jobName).append('\'');
+        sb.append("}");
+        return sb.toString();
+    }
 }
--- studyplatform/src/main/java/com/wps/studyplatform/job/scheduler/DynamicSchedulerFactory.java
+++ studyplatform/src/main/java/com/wps/studyplatform/job/scheduler/DynamicSchedulerFactory.java
@@ -1,9 +1,12 @@
 package com.wps.studyplatform.job.scheduler;
 
-import org.quartz.Scheduler;
+import io.jsonwebtoken.lang.Assert;
+import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+
+import java.util.Date;
 
 /**
  * @Title DynamicSchedulerFactory
@@ -18,7 +21,72 @@ public class DynamicSchedulerFactory implements InitializingBean {
     public DynamicSchedulerFactory(){
 
     }
-    public static boolean registerJob()
+    public static boolean registerJob(DynamicJob job)throws SchedulerException{
+        final TriggerKey triggerKey =job.triggerKey();
+        if (scheduler.checkExists(triggerKey)){
+            final Trigger trigger=scheduler.getTrigger(triggerKey);
+            throw new SchedulerException(trigger+"已经存在");
+        }
+        final CronTrigger cronTrigger=job.cronTrigger();
+        final JobDetail jobDetail=job.jobDetail();
+        final Date date=scheduler.scheduleJob(jobDetail,cronTrigger);
+        LOG.debug("Register DynamicJoB {} on [{}]",job,date);
+        return true;
+    }
+
+    /**
+     * check the job is exist or not
+     * @throws Exception
+     */
+    public static boolean existJob(DynamicJob job) throws SchedulerException {
+        final TriggerKey triggerKey=job.triggerKey();
+        return scheduler.checkExists(triggerKey);
+    }
+    public static boolean pauseJob(DynamicJob existJob) throws SchedulerException {
+        final TriggerKey triggerKey=existJob.triggerKey();
+        boolean result=false;
+        if (scheduler.checkExists(triggerKey)){
+            scheduler.pauseTrigger(triggerKey);
+            result=true;
+            LOG.debug("Pause exist DynamicJob {},triggerKey [{}] successful",existJob,triggerKey);
+        }else {
+            LOG.debug("Failed pause exist DynamicJob {},because not fount triggerKey [{}]",existJob,triggerKey);
+        }
+        return result;
+    }
+
+    public static boolean resumeJob(DynamicJob existJob) throws SchedulerException {
+        final TriggerKey triggerKey=existJob.triggerKey();
+        boolean result=false;
+        if (scheduler.checkExists(triggerKey)){
+            final CronTrigger newTrigger=existJob.cronTrigger();
+            final Date date=scheduler.rescheduleJob(triggerKey,newTrigger);
+            result=true;
+            LOG.debug("Resume exist DynamicJob {},triggerKey [{}] on [{}] successful",existJob,triggerKey,date);
+        }else {
+            LOG.debug("Failed resume exist DynamicJob {},because not fount triggerKey [{}]",existJob,triggerKey);
+        }
+        return result;
+    }
+
+    /**
+     * 删除吊已经存在的job
+     * @param existJob
+     * @return
+     * @throws SchedulerException
+     */
+    public static boolean removeJob(DynamicJob existJob) throws SchedulerException {
+        final TriggerKey triggerKey=existJob.triggerKey();
+        boolean result=false;
+        if (scheduler.checkExists(triggerKey)){
+            result=scheduler.unscheduleJob(triggerKey);
+        }
+        LOG.debug("Remove DynamicJob {} result [{}]",existJob,result);
+        return result;
+    }
+    public void setScheduler(Scheduler scheduler){
+        DynamicSchedulerFactory.scheduler=scheduler;
+    }
 
 
 
@@ -26,6 +94,8 @@ public class DynamicSchedulerFactory implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        Assert.notNull(scheduler,"scheduler is null");
+        LOG.info("initial dynamicSchedulerFactory successful,scheduler instance:{}",scheduler);
 
     }
 }
