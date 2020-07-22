171516018821f3e92c74e34e6349cef97d00a02d
w21095 <1169114186@qq.com>
2020-07-22 17:24:39 +0800

    study-rabbitmq 工作模式（多个消费之，负载均衡获得消息）

---
+++ study-rabbitmq/src/main/java/com/wps/studyrabbitmq/consumer/MessageConsumerSecond.java
@@ -0,0 +1,23 @@
+package com.wps.studyrabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title MessageConsumerSecond
+ * @Description
+ * @auther wps
+ * @Date 2020/7/2217:16
+ */
+@Component
+public class MessageConsumerSecond {
+
+    //监听器监听指定的Queue
+    @RabbitListener(queues = "hello")
+    public void process(String hello){
+        System.out.println("===============消费者2========"+hello);
+
+    }
+}
