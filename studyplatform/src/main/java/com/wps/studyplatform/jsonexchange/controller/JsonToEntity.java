6cd08fd0a47c2283429c6b89ca44c3cad506909c
w21095 <1169114186@qq.com>
2020-07-27 20:00:20 +0800

    studyplatform 复杂json转换方法

--- studyplatform/src/main/java/com/wps/studyplatform/jsonexchange/controller/JsonToEntity.java
+++ studyplatform/src/main/java/com/wps/studyplatform/jsonexchange/controller/JsonToEntity.java
@@ -4,6 +4,7 @@ import com.wps.studyplatform.jsonexchange.entity.Share;
 import com.wps.studyplatform.jsonexchange.entity.SysUserEntity;
 import com.wps.studyplatform.jsonexchange.utils.JsonUtils;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 /**
@@ -15,6 +16,18 @@ import java.util.Optional;
 public class JsonToEntity {
 
     public static void main(String[] args) {
+        //调用其他服务的接口时，返回的数据类型为：HashMap，其中保存的为<String,List>
+        /**
+         * 裁controoler层接收时可以使用Object，解决方法有两个
+         * 1：使用JsonUtils.readValue转成HashMap对象
+         *  Object a="";
+         *  Optional<HashMap> shareOptional=JsonUtils.readValue(a.toString(),HashMap.class);
+         * 2：强转成HashMap对象：
+         * Object a="";
+         * HashMap<String,Object> hashMap= (HashMap<String, Object>) a;
+         *
+         */
+
 
         String json="{\"type\":\"1\",\"title\":\"母亲节\",\"url\":\"\",\"content\":\"\",\"endTime\":\"\",\"questionModels\":[{\"title\":\"标题一\",\"type\":2,\"options\":[{\"option\":\"选项一\"},{\"option\":\"选项二\"}]}]}";
         Optional<Share> shareOptional=JsonUtils.readValue(json,Share.class);
