7c19ae0a471524422d0537c800a49540f4659c3f
w21095 <1169114186@qq.com>
2020-12-28 21:24:05 +0800

    study-redis项目 搭建项目架构

---
+++ study-redis/src/main/java/com/wps/studyredis/controller/RedisController.java
@@ -0,0 +1,13 @@
+package com.wps.studyredis.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title RedisController
+ * @Description
+ * @auther wps
+ * @Date 2020/8/1320:26
+ */
+@RestController
+public class RedisController {
+}
